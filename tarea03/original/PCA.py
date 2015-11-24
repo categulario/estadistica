d2a8523fe7eb5e21b76be778735ee079779cfc48
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-11-24 17:25:40 -0600

    update PCA

--- tarea03/original/PCA.py
+++ tarea03/original/PCA.py
@@ -1,15 +1,37 @@
+#!/usr/bin/env python3
 import numpy as np
 import pandas as pd
 from names import names
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     data = pd.read_csv('spambase.data', names = names)
 
+    # Remove last column (the class)
+    data = data.loc[:,names[:-1]]
+
     # Normalize data
-    for name in names:
-        data[name] -= data[name].mean()
+    data -= data.mean()
+    data /= data.std()
 
     # Calculate covariance matrix
     cov = data.cov()
 
-    values, vectors = np.linalg.eig(cov)
+    # gives eigenvectors and eigenvalues, sorted ascending
+    values, vectors = np.linalg.eigh(cov)
+
+    # Sort ascending
+    dim = len(values)
+    for i in range(dim//2):
+        j = dim - i - 1
+        values[i], values[j] = values[j], values[i]
+
+        aux = vectors[:,i].copy()
+        vectors[:,i] = vectors[:,j]
+        vectors[:,j] = aux
+
+    plt.plot(values, 'o')
+    plt.show()
+
+    # Get dimention of final space
+    p = int(input('¿Cuántos vectores quieres? '))
