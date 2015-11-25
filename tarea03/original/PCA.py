c90b3376d7d09b998ff523d5f876606a29efcd71
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-11-24 20:08:16 -0600

    finish PCA

--- tarea03/original/PCA.py
+++ tarea03/original/PCA.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import numpy as np
 import pandas as pd
 from names import names
@@ -8,7 +9,8 @@ if __name__ == '__main__':
     data = pd.read_csv('spambase.data', names = names).sample(frac=1/3)
 
     # Remove last column (the class)
-    data = data.loc[:,names[:-1]]
+    classes = data.loc[:,names[-1]]
+    data    = data.loc[:,names[:-1]]
 
     # Normalize data
     data -= data.mean()
@@ -30,11 +32,38 @@ if __name__ == '__main__':
         vectors[:,i] = vectors[:,j]
         vectors[:,j] = aux
 
-    plt.plot(values, 'o')
-    plt.show()
-
     # Get dimention of final space
-    p = int(input('¿Cuántos vectores quieres? '))
+    if '--ask' in sys.argv:
+        plt.plot(values, 'o')
+        plt.show()
 
-    # Compute the feature vector
+        p = int(input('¿Cuántos vectores quieres? '))
+    else:
+        p = 2
+
+    # Compute the feature vector matrix (dim x p)
     feature = vectors[:,:p]
+
+    # Finally compute new data multiplying feature vector matrix with
+    # data matrix
+    final = np.dot(feature.T, data.T).T
+
+    # Add the class column
+    reduced_data = np.append(final, classes.reshape((len(classes), 1)), axis=1)
+
+    # Save the reduced-dimention data to a file
+    formats = ('%f',)*p + ('%d',)
+    np.savetxt("reduced.csv", reduced_data, delimiter=",", fmt=formats)
+
+    # plot the new data
+    df = pd.DataFrame(reduced_data, columns=('x', 'y', 'class'))
+    spam = df[df['class'] == 1]
+    nospam = df[df['class'] == 0]
+
+    if p == 2:
+        plt.plot(spam['x'], spam['y'], 'ro', label='spam')
+        plt.plot(nospam['x'], nospam['y'], 'bo', label='nospam')
+
+        plt.legend(loc='best')
+
+        plt.show()
