1564eaeeef8e6c86a8225a14cb448fe0b3516d84
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-11-25 12:52:47 -0600

    finish clustering

--- final/k_mean.py
+++ final/k_mean.py
@@ -1,4 +1,63 @@
 #!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+CLUSTERS = 3
+
+def euclidean(p1, p2):
+    return np.sqrt((p1['x']-p2['x'])**2 + (p1['y'] - p2['y'])**2)
+
+def closest_cluster(row, centroids):
+    closest  = 0
+    distance = euclidean(row, centroids[0])
+
+    for cluster in range(1, CLUSTERS):
+        new_distance = euclidean(row, centroids[cluster])
+
+        if new_distance < distance:
+            closest  = cluster
+            distance = new_distance
+
+    return closest
 
 if __name__ == '__main__':
-    print('hola')
+    data = pd.read_csv('data.csv', names=('x', 'y'))
+
+    # asign a random cluster to each observation
+    data['cluster'] = np.random.randint(CLUSTERS, size=len(data['x']))
+
+    times   = 0
+
+    while True:
+        changes = False
+        # Compute each cluster's centroid
+        centroids = {
+            i: data[data['cluster'] == i].loc[:,['x', 'y']].mean()
+            for i in range(CLUSTERS)
+        }
+
+        # Assign each observation the class of the closest centroid
+        for i, row in data.iterrows():
+            closest = closest_cluster(row, centroids)
+            if closest != row['cluster']:
+                data.loc[i, 'cluster'] = closest
+                changes = True
+
+        times += 1
+
+        if not changes:
+            break
+
+    print('clustering finished after %d iterations'%times)
+    print(data)
+
+    # plot the clusters
+    for c in range(CLUSTERS):
+        plt.plot(
+            data[data['cluster'] == c]['x'],
+            data[data['cluster'] == c]['y'],
+            'o'
+        )
+
+    plt.show()
